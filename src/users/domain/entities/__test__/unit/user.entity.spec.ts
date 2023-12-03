42f73c707a9536e1bac6b983534e6675ec942cc9
Felipe Moura <felipemimoura@gmail.com>
2023-12-03 05:56:31 -0300

    refactor: use builder instead of hardcoding

--- src/users/domain/entities/__test__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__test__/unit/user.entity.spec.ts
@@ -1,15 +1,10 @@
-import { faker } from '@faker-js/faker'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
 import { UserEntity, UserProps } from '../../user.entity'
 describe('User Entity test', () => {
   let props: UserProps
   let sut: UserEntity
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    }
-
+    props = UserDataBuilder({})
     sut = new UserEntity(props)
   })
 
