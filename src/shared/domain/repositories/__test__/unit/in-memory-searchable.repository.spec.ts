ab969801ce231236156d359a03514bc2b3140a1f
Felipe Moura <felipemimoura@gmail.com>
2023-12-29 08:50:31 -0300

    test: apply filter method test

--- src/shared/domain/repositories/__test__/unit/in-memory-searchable.repository.spec.ts
+++ src/shared/domain/repositories/__test__/unit/in-memory-searchable.repository.spec.ts
@@ -31,7 +31,41 @@ describe('InMemorySearchableRepository unit test', () => {
     sut = new StubInMemorySearchableRepository()
   })
 
-  describe('apply filter method', () => {})
+  describe('apply filter method', () => {
+    it('should no filter when filter params is null', async () => {
+      const items = [new StubEntity({ name: 'name value', price: 10 })]
+      const spyFilterMethod = jest.spyOn(items, 'filter')
+
+      const itemsFiltered = await sut['applyFilter'](items, null)
+
+      expect(itemsFiltered).toStrictEqual(items)
+      expect(spyFilterMethod).not.toHaveBeenCalled()
+    })
+
+    it('should filter using a filter params', async () => {
+      const items = [
+        new StubEntity({ name: 'test', price: 10 }),
+        new StubEntity({ name: 'TEST', price: 10 }),
+        new StubEntity({ name: 'fake', price: 10 }),
+      ]
+      const spyFilterMethod = jest.spyOn(items, 'filter')
+
+      let itemsFiltered = await sut['applyFilter'](items, 'TEST')
+
+      expect(itemsFiltered).toStrictEqual([items[0], items[1]])
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1)
+
+      itemsFiltered = await sut['applyFilter'](items, 'test')
+
+      expect(itemsFiltered).toStrictEqual([items[0], items[1]])
+      expect(spyFilterMethod).toHaveBeenCalledTimes(2)
+
+      itemsFiltered = await sut['applyFilter'](items, 'no-filter')
+
+      expect(itemsFiltered).toHaveLength(0)
+      expect(spyFilterMethod).toHaveBeenCalledTimes(3)
+    })
+  })
   describe('apply sort method', () => {})
   describe('apply paginate method', () => {})
   describe('search method', () => {})
